3ec0641d6d851d3328734f6d1d08059663f11548
naccyworld <nnamchitony@gmail.com>
2023-06-13 15:13:03 +0100

    Modify the ci.yaml file

--- airline/settings.py
+++ airline/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'flightDB', 
         'USER': 'postgres',
         'PASSWORD': 'badnaccy13',
-        'HOST': '127.0.0.1', 
+        'HOST': 'localhost', 
         'PORT': '5432',
     }
 }
